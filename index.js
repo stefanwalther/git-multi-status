511c1a15d714af1d69a28f884e81d0fa23631838
Stefan Walther <Stefan.Walther@qlik.com>
2016-02-28 18:43:49 +0100

    Add basic formatting

--- index.js
+++ index.js
@@ -1,26 +1,30 @@
-var exec = require('child_process').exec,
-	fs =  require('fs');
+var exec = require( 'child_process' ).exec;
+var fs = require( 'fs' );
+var chalk = require( 'chalk' );
+var repeat = require('repeat-string');
 
-module.exports = function() {
-	var files = fs.readdirSync(process.cwd());
-	files.forEach(function(f) {
-		if(fs.statSync(f).isDirectory()) {
-			exec('git status', { cwd: f }, function (error, stdout, stderr) {
-				console.log(f);
+module.exports = function () {
+	var files = fs.readdirSync( process.cwd() );
+	files.forEach( function ( f ) {
+		if ( fs.statSync( f ).isDirectory() ) {
+			exec( 'git status', {cwd: f}, function ( error, stdout, stderr ) {
 
-				if(error) {
-					console.error(error);
+				console.log( chalk.gray( repeat('-', 60) ) );
+				console.log( chalk.green( f ) );
+
+				if ( error ) {
+					console.error( error );
 					return;
 				}
 
-				if(stderr) {
-					console.error(stderr);
+				if ( stderr ) {
+					console.error( stderr );
 					return;
 				}
 
-				console.log(stdout);
+				console.log( stdout );
 				return;
-			});
+			} );
 		}
-	});
+	} );
 };
